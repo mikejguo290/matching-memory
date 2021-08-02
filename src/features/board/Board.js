ad7fcc446af4e045523ad2fa34cdbd23a8c74a27
mikejguo290 <mikejguo290@gmail.com>
2021-08-02 21:41:20 +0100

    complete getRowCard function to select cards in order from currentBoard to populate <CardRow cards={rowCards} />.

--- src/features/board/Board.js
+++ src/features/board/Board.js
@@ -7,17 +7,23 @@ import { selectBoard } from './boardSlice'
 export const Board = () => {
   // Add selected data variable and implement below
   const currentBoard = useSelector(selectBoard);
+  /* The logic in the Board component creates a grid of cards by rendering a calculated number of CardRow components. 
+  To finish the implementation you will use the data in currentBoard to help calculate the number of CardRow components 
+  and then create an array of card objects for each row. */
+  /* at least describe, if not show me what I'm supposed to implement CodeCademy!? */
 
-  const numberOfCards = 0;
+  const numberOfCards = currentBoard.length();
   const columns = 3;
   const rows = Math.floor(numberOfCards / columns);
 
   const getRowCards = (row) => {
+    // select cards in order from currentBoard to populate rows. content.push(<CardRow key={row} cards={rowCards} />
+    // where each row has three cards - from the columns=3;
     const rowCards = [];
     for (let j = 0; j < columns; j++) {
-      const cardIndex = row * columns + j;
+      const cardIndex = row * columns + j; // row 0: cardIndex=[0,1,2],  row 1: cardIndex=[3,4,5],  cardIndex=row 2:[6,7,8] 
       // Implement selected data below
-      rowCards.push({});
+      rowCards.push(currentBoard[cardIndex]);
     }
     return rowCards;
   };
