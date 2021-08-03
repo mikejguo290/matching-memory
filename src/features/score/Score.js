692dd2be1b799aacab42b3174901253ea62aca86
mikejguo290 <mikejguo290@gmail.com>
2021-08-03 15:12:46 +0100

    the Score component keep score using the number of matched cards. import useSelector & selectMatechedIDs from boardSlice.

--- src/features/score/Score.js
+++ src/features/score/Score.js
@@ -1,9 +1,14 @@
 import React from 'react';
 // Add import statement below
+import { useSelector } from 'react-redux'
 
+// import selectors;
+import { selectMatchedIDs } from '../board/boardSlice.js';
 
 export const Score = () => {
+  // score is based on the number of cards matched
   // Add selected data variable below
+
   
 
   return (
